89d553adf7d606ff25548235e70e3bc52dfa9215
Samantha Healy <samhealy09@gmail.com>
2015-03-04 15:44:39 -0500

    fixing errors

--- app/scripts/album.js
+++ app/scripts/album.js
@@ -1,46 +1,46 @@
 // Example Album
- var albumPicasso = {
-   name: 'The Colors',
-   artist: 'Pablo Picasso',
-   label: 'Cubism',
-   year: '1881',
-   albumArtUrl: '/images/album-placeholder.png',
-   songs: [
-       { name: 'Blue', length: '4:26' },
-       { name: 'Green', length: '3:14' },
-       { name: 'Red', length: '5:01' },
-       { name: 'Pink', length: '3:21'},
-       { name: 'Magenta', length: '2:15'}
-     ]
- };
- 
- // Another Example Album
- var albumMarconi = {
-   name: 'The Telephone',
-   artist: 'Guglielmo Marconi',
-   label: 'EM',
-   year: '1909',
-   albumArtUrl: '/images/album-placeholder.png',
-   songs: [
-       { name: 'Hello, Operator?', length: '1:01' },
-       { name: 'Ring, ring, ring', length: '5:01' },
-       { name: 'Fits in your pocket', length: '3:21'},
-       { name: 'Can you hear me now?', length: '3:14' },
-       { name: 'Wrong phone number', length: '2:15'}
-     ]
- };
+var albumPicasso = {
+	name: 'The Colors',
+	artist: 'Pablo Picasso',
+	label: 'Cubism',
+	year: '1881',
+	albumArtUrl: '/images/album-placeholder.png',
+	songs: [
+	{ name: 'Blue', length: '4:26' },
+	{ name: 'Green', length: '3:14' },
+	{ name: 'Red', length: '5:01' },
+	{ name: 'Pink', length: '3:21'},
+	{ name: 'Magenta', length: '2:15'}
+	]
+};
+
+// Another Example Album
+var albumMarconi = {
+	name: 'The Telephone',
+	artist: 'Guglielmo Marconi',
+	label: 'EM',
+	year: '1909',
+	albumArtUrl: '/images/album-placeholder.png',
+	songs: [
+	{ name: 'Hello, Operator?', length: '1:01' },
+	{ name: 'Ring, ring, ring', length: '5:01' },
+	{ name: 'Fits in your pocket', length: '3:21'},
+	{ name: 'Can you hear me now?', length: '3:14' },
+	{ name: 'Wrong phone number', length: '2:15'}
+	]
+};
 
 var createSongRow = function(songNumber, songName, songLength) {
-   var template =
-       '<tr>'
-     + '  <td class="col-md-1">' + songNumber + '</td>'
-     + '  <td class="col-md-9">' + songName + '</td>'
-     + '  <td class="col-md-2">' + songLength + '</td>'
-     + '</tr>'
-     ;
- 
-   return $(template);
- };
+	var template =
+	'<tr>'
+	+ '  <td class="col-md-1">' + songNumber + '</td>'
+	+ '  <td class="col-md-9">' + songName + '</td>'
+	+ '  <td class="col-md-2">' + songLength + '</td>'
+	+ '</tr>'
+	;
+
+	return $(template);
+};
 
 var changeAlbumView = function(album) {
    // Update the album title
@@ -72,11 +72,11 @@ var changeAlbumView = function(album) {
  };
 
 // This 'if' condition is used to prevent the jQuery modifications
- // from happening on non-Album view pages.
- //  - Use a regex to validate that the url has "/album" in its path.
- if (document.URL.match(/\/album.html/)) {
-   // Wait until the HTML is fully processed.
-   $(document).ready(function() {
+// from happening on non-Album view pages.
+//  - Use a regex to validate that the url has "/album" in its path.
+if (document.URL.match(/\/album.html/)) {
+// Wait until the HTML is fully processed.
+	$(document).ready(function() {
 		changeAlbumView(albumPicasso);
- });
+	});
 }
