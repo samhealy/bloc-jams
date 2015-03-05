628f3edb2e215804e57e392f2c0598882223758d
Samantha Healy <samhealy09@gmail.com>
2015-03-04 19:24:31 -0500

    debugging

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -91,70 +91,152 @@
   globals.require.brunch = true;
 })();
 require.register("scripts/album", function(exports, require, module) {
+// Example Album
+var albumPicasso = {
+	name: 'The Colors',
+	artist: 'Pablo Picasso',
+	label: 'Cubism',
+	year: '1881',
+	albumArtUrl: '/images/album-placeholder.png',
+	songs: [
+	{ name: 'Blue', length: '4:26' },
+	{ name: 'Green', length: '3:14' },
+	{ name: 'Red', length: '5:01' },
+	{ name: 'Pink', length: '3:21'},
+	{ name: 'Magenta', length: '2:15'}
+	]
+};
 
+// Another Example Album
+var albumMarconi = {
+	name: 'The Telephone',
+	artist: 'Guglielmo Marconi',
+	label: 'EM',
+	year: '1909',
+	albumArtUrl: '/images/album-placeholder.png',
+	songs: [
+	{ name: 'Hello, Operator?', length: '1:01' },
+	{ name: 'Ring, ring, ring', length: '5:01' },
+	{ name: 'Fits in your pocket', length: '3:21'},
+	{ name: 'Can you hear me now?', length: '3:14' },
+	{ name: 'Wrong phone number', length: '2:15'}
+	]
+};
+
+var createSongRow = function(songNumber, songName, songLength) {
+	var template =
+	'<tr>'
+	+ '  <td class="col-md-1">' + songNumber + '</td>'
+	+ '  <td class="col-md-9">' + songName + '</td>'
+	+ '  <td class="col-md-2">' + songLength + '</td>'
+	+ '</tr>'
+	;
+
+	return $(template);
+};
+
+var changeAlbumView = function(album) {
+// Update the album title
+var $albumTitle = $('.album-title');
+$albumTitle.text(album.name);
+
+// Update the album artist
+var $albumArtist = $('.album-artist');
+$albumArtist.text(album.artist);
+
+// Update the meta information
+var $albumMeta = $('.album-meta-info');
+$albumMeta.text(album.year + " on " + album.label);
+
+// Update the album image
+var $albumImage = $('.album-image img');
+$albumImage.attr('src', album.albumArtUrl);
+
+// Update the Song List
+var $songList = $(".album-song-listing");
+$songList.empty();
+var songs = album.songs;
+for (var i = 0; i < songs.length; i++) {
+	var songData = songs[i];
+	var $newRow = createSongRow(i + 1, songData.name, songData.length);
+	$songList.append($newRow);
+}
+
+};
+
+// This 'if' condition is used to prevent the jQuery modifications
+// from happening on non-Album view pages.
+//  - Use a regex to validate that the url has "/album" in its path.
+if (document.URL.match(/\/album.html/)) {
+// Wait until the HTML is fully processed.
+$(document).ready(function() {
+	changeAlbumView(albumPicasso);
+});
+}
 });
 
 ;require.register("scripts/app", function(exports, require, module) {
 require("./landing");
 require('./collection');
+require('./album');
 });
 
 ;require.register("scripts/collection", function(exports, require, module) {
 var buildAlbumThumbnail = function() {
-    var template =
-        '<div class="collection-album-container col-md-2">'
-      + '  <img src="/images/album-placeholder.png"/>'
-      + '  <div class="caption album-collection-info">'
-      + '    <p>'
-      + '      <a class="album-name" href="/album.html"> Album Name </a>'
-      + '      <br/>'
-      + '      <a href="/album.html"> Artist name </a>'
-      + '      <br/>'
-      + '      X songs'
-      + '      <br/>'
-      + '    </p>'
-      + '  </div>'
-      + '</div>';
- 
-   return $(template);
- };
+	var template =
+	'<div class="collection-album-container col-md-2">'
+	+ '  <img src="/images/album-placeholder.png"/>'
+	+ '  <div class="caption album-collection-info">'
+	+ '    <p>'
+	+ '      <a class="album-name" href="/album.html"> Album Name </a>'
+	+ '      <br/>'
+	+ '      <a href="/album.html"> Artist name </a>'
+	+ '      <br/>'
+	+ '      X songs'
+	+ '      <br/>'
+	+ '    </p>'
+	+ '  </div>'
+	+ '</div>';
+
+	return $(template);
+};
 
 var updateCollectionView = function() {
-   var $collection = $(".collection-container .row");
-   $collection.empty();
- 
-   for (var i = 0; i < 33; i++) {
-     var $newThumbnail = buildAlbumThumbnail();
-     $collection.append($newThumbnail);
-   }
- };
+	var $collection = $(".collection-container .row");
+	$collection.empty();
+
+	for (var i = 0; i < 33; i++) {
+		var $newThumbnail = buildAlbumThumbnail();
+		$collection.append($newThumbnail);
+	}
+};
 
 if (document.URL.match(/\/collection.html/)) {
-   // Wait until the HTML is fully processed.
-   $(document).ready(function() {
-		updateCollectionView();
-   });
- }
+// Wait until the HTML is fully processed.
+$(document).ready(function() {
+	updateCollectionView();
+});
+}
 });
 
 ;require.register("scripts/landing", function(exports, require, module) {
 $(document).ready(function() { 
-   $('.hero-content h3').click(function(){
-   	var subText = $(this).text();
-      $(this).text(subText + "!");
-   });
+  $('.hero-content h3').click(function(){
+    var subText = $(this).text();
+    $(this).text(subText + "!");
+  });
 
-var onHoverAction = function(event) {
-     console.log('Hover action triggered.');
-     $(this).animate({'margin-top': '10px'});
-   };
- 
-   var offHoverAction = function(event) {
-     console.log('Off-hover action triggered.');
-     $(this).animate({'margin-top': '0px'});
-   };
- 
-   $('.selling-points .point').hover(onHoverAction, offHoverAction);
+  var onHoverAction = function(event) {
+    console.log('Hover action triggered.');
+    $(this).animate({'margin-top': '10px'});
+  };
+
+  var offHoverAction = function(event) {
+    console.log('Off-hover action triggered.');
+    $(this).animate({'margin-top': '0px'});
+  };
+
+  $('.selling-points .point').hover(onHoverAction, offHoverAction);
 });
 });
 
