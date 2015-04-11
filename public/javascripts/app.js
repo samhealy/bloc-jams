118834cb65fe5112475cc9c73920e68f61125288
Samantha Healy <samhealy09@gmail.com>
2015-04-11 10:58:15 -0400

    Built a SongPlayer service to share song data across views.

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -354,11 +354,10 @@ $scope.albumURLs = [
 
  }]);
 
- blocJams.controller('Album.controller', ['$scope', function($scope) {
+ blocJams.controller('Album.controller', ['$scope', 'SongPlayer', function($scope, SongPlayer) {
    $scope.album = angular.copy(albumPicasso);
 
    var hoveredSong = null;
-   var playingSong = null;
  
    $scope.onHoverSong = function(song) {
      hoveredSong = song;
@@ -369,7 +368,7 @@ $scope.albumURLs = [
    };
 
    $scope.getSongState = function(song) {
-     if (song === playingSong) {
+     if (song === SongPlayer.currentSong && SongPlayer.playing) {
        return 'playing';
      }
      else if (song === hoveredSong) {
@@ -379,13 +378,39 @@ $scope.albumURLs = [
    };
 
    $scope.playSong = function(song) {
-      playingSong = song;
-    };
+     SongPlayer.setSong($scope.album, song);
+     SongPlayer.play();
+   };
  
     $scope.pauseSong = function(song) {
-      playingSong = null;
+      SongPlayer.pause();
     };
  }]);
+
+blocJams.controller('PlayerBar.controller', ['$scope', 'SongPlayer', function($scope, SongPlayer) {
+   $scope.songPlayer = SongPlayer;
+ }]);
+
+blocJams.service('SongPlayer', function() {
+   return {
+     currentSong: null,
+     currentAlbum: null,
+     playing: false,
+ 
+     play: function() {
+       this.playing = true;
+     },
+     pause: function() {
+       this.playing = false;
+     },
+     setSong: function(album, song) {
+       this.currentAlbum = album;
+       this.currentSong = song;
+     }
+   };
+ });
+
+
 });
 
 ;require.register("scripts/collection", function(exports, require, module) {
